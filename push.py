cab552a8b3c9de47329896d18b4c58585accdebf
quinn <maninios@163.com>
2021-03-16 20:34:26 +0800

    添加push脚本

---
+++ push.py
@@ -0,0 +1,24 @@
+import os
+
+def push():
+
+    checkCommand = "pod spec lint SpeedySwift.podspec --verbose --allow-warnings"
+    pushCommand = "pod trunk push SpeedySwift.podspec --verbose --allow-warnings --allow-warnings"
+    updatePirvateRepo = "pod repo update CTSpecs"
+    checkRet = os.system(checkCommand)
+    if checkRet != 0:
+        print("校验出错，请检查spec文件是否配置正确")
+        exit - 1
+    else:
+        pushRet = os.system(pushCommand)
+        if pushRet != 0:
+            print("上传失败！")
+            exit - 1
+        else:
+            print("上传完成！！")
+            os.system(updatePirvateRepo)
+    return
+
+
+if __name__ == "__main__":
+	push()
