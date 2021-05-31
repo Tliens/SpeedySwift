22229f0ba445c41f14f9263d0b40a9fade8d53d6
Quinn <quinn_x@126.com>
2021-06-01 00:00:31 +0800

    添加不响应界面

---
+++ SpeedySwift/Classes/Base/SSContentView.swift
@@ -0,0 +1,20 @@
+//
+//  SSContentView.swift
+//  SpeedySwift
+//
+//  Created by Quinn on 2021/5/31.
+//
+
+import UIKit
+/// 不响应事件
+public class SSContentView: UIView {
+
+    override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
+        let view = super.hitTest(point, with: event);
+        if (view == self) {
+            return nil
+        }
+        return view
+    }
+
+}
