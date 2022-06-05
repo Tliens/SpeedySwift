97327c099504ea12e2648ff1844b62880f62aac1
quinn <maninios@163.com>
2022-06-05 20:20:42 +0800

    支持xib

--- SpeedySwift/Classes/Base/SSCollectionViewLayout.swift
+++ SpeedySwift/Classes/Base/SSCollectionViewLayout.swift
@@ -31,7 +31,7 @@ open class SSCollectionViewLayout: UICollectionViewFlowLayout {
     }
     
     public required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: coder)
     }
     
 }
